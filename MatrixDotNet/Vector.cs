406e9878095b5e9a223f0d282a327d4328809596
kidfruit <kidfruit@gmail.com>
2011-12-12 17:59:28 +0800

    Initial

---
+++ MatrixDotNet/Vector.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MatrixDotNet
+{
+    public class Vector:List<double>
+    {
+        public Vector():base(){}
+        public Vector(params double[] vals):base()
+        {
+            base.AddRange(vals);
+        }
+        public void ResetValue(params double[] vals)
+        {
+            base.Clear();
+            base.AddRange(vals);
+        }
+
+        public Vector SubVector(int beginIndex, int endIndex)
+        {
+            double[] temp = new double[endIndex - beginIndex + 1];
+            base.GetRange(beginIndex, endIndex - beginIndex + 1).CopyTo(temp);
+            return new Vector(temp);
+        }
+
+        public static Vector operator+(Vector v1,Vector v2)
+        {
+            if (v1.Count!=v2.Count)
+            {
+                throw new Exception("Vector is not compatible");
+            }
+            else
+            {
+                double[] results = new double[v1.Count];
+                for (int i = 0; i < v2.Count; i++)
+                {
+                    results[i] = v1[i] + v2[i];
+                }
+                return new Vector(results);
+            }
+        }
+        public static Vector operator -(Vector v1, Vector v2)
+        {
+            if (v1.Count != v2.Count)
+            {
+                throw new Exception("Vector is not compatible");
+            }
+            else
+            {
+                double[] results = new double[v1.Count];
+                for (int i = 0; i < v2.Count; i++)
+                {
+                    results[i] = v1[i] - v2[i];
+                }
+                return new Vector(results);
+            }
+        }
+    }
+}
---
+++ TestForMatrix/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MatrixDotNet;
+
+namespace TestForMatrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vector a = new Vector(new double[] { 1, 2, 3 });
+            a.Add(4);
+            a.AddRange(new double[] { 5, 6, 7 });
+            bool x = a.Contains(4);
+            Vector b = a;
+            Vector c = a.SubVector(1, 3);
+            Vector d = a + b;
+            Matrix m1 = new Matrix(new double[,] { { 1, 2, 3 }, { 4, 5, 6 } });
+            Matrix m2 = new Matrix(new double[,] { { 1, 2, 3 }, { 4, 5, 6 } });
+            Matrix m3 = new Matrix();
+            m3 = m1 - m2;
+
+            Console.WriteLine(a.Count);
+        }
+    }
+}
