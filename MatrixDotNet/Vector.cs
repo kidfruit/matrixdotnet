de7adae1cb988f1d330f9d2f72b02ff693aa5eba
kidfruit <kidfruit@gmail.com>
2012-02-24 17:55:35 +0800

    add test project

--- MatrixDotNet/Vector.cs
+++ MatrixDotNet/Vector.cs
@@ -6,7 +6,7 @@ using System.Text;
 
 namespace MatrixDotNet
 {
-    public class Vector
+    public class Vector:ICloneable
     {
         private double[] values;
         public Vector(int length)
@@ -60,7 +60,7 @@ namespace MatrixDotNet
             Vector result = new Vector(length);
             for (int i = startIndex; i < startIndex + length; i++)
             {
-                result[i] = this[i];
+                result[i-startIndex] = this[i];
             }
             return result;
 
@@ -99,7 +99,53 @@ namespace MatrixDotNet
             }
         }
 
+        public static bool operator!=(Vector v1,Vector v2)
+        {
+            return !object.Equals(v1, v2);
+        }
+        public static bool operator ==(Vector v1, Vector v2)
+        {
+            return object.Equals(v1, v2);
+        }
+        public override bool Equals(object o)
+        {
+            Vector v = o as Vector;
+            if (v == null)
+            {
+                return false;
+            }
+            if (this.Count != v.Count)
+            {
+                throw new Exception("Vector is not compatible");
+            }
+            else
+            { 
+                if( this.GetHashCode() != v.GetHashCode())
+                    return false;
+                for (int i = 0; i < v.Count;i++ )
+                {
+                    if (this[i]!=v[i])
+                    {
+                        return false;
+                    }
+                }
+                return true;
+               
+            }
+        }
+        public override int GetHashCode()
+        {
+            int hash = 0;
+            foreach (double x in values)
+            {
+                hash += x.GetHashCode();
+            }
+            return hash;
+        }
 
-
+        public object Clone()
+        {
+            return new Vector(this.values);
+        }
     }
 }
